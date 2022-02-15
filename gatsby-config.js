93571831af3151eca05a1615db3a317cbda87cc5
Eddiebee <edbeeotudor@gmail.com>
2022-02-15 20:52:52 +0100

    install required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,54 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+// .env setup
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Bee Blog",
+    description: "Honey sweet blogs",
+    author: "@eddiebee",
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600"],
+            },
+          ],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `l57lubhs2ydc`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
