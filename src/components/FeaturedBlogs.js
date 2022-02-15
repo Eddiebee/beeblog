74c34e227337d10edfb10d8ce3a0e56a3dc6de4c
Eddiebee <edbeeotudor@gmail.com>
2022-02-15 20:58:34 +0100

    build featured blogs with static query

--- src/components/FeaturedBlogs.js
+++ src/components/FeaturedBlogs.js
@@ -1,29 +1,93 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import slugify from "slugify"
 import styled from "styled-components"
 import Blog from "./Blog"
+
+const query = graphql`
+  {
+    allContentfulBlogPost(
+      filter: { featured: { eq: true } }
+      sort: { fields: date, order: ASC }
+    ) {
+      nodes {
+        title
+        id
+        date(fromNow: true)
+        author {
+          name
+          avatar {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          }
+        }
+        tags {
+          tags
+        }
+        content {
+          raw
+        }
+        banner {
+          gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+    }
+  }
+`
+
+const FeaturedBlogs = () => {
+  const data = useStaticQuery(query)
+  const blogs = data.allContentfulBlogPost.nodes
+  return (
+    <Wrapper>
+      <h1>Featured Blogs</h1>
+      <main>
+        {blogs.map(
+          ({
+            id,
+            title,
+            date,
+            author,
+            banner,
+            tags: { tags },
+            content: { raw },
+          }) => {
+            const hashTags = tags.map(tag => `#${tag}`)
+            const rawDocument = JSON.parse(raw)
+            const slug = slugify(title, {
+              lower: true,
+              // strict: true,
+              remove: /[*+~.()'"!:@]/g,
+            })
+            return (
+              <Link to={`/blogs/${slug}`} key={id}>
+                <Blog
+                  title={title}
+                  date={date}
+                  author={author}
+                  banner={banner}
+                  tags={hashTags.join(" ")}
+                  content={documentToReactComponents(rawDocument)}
+                />
+              </Link>
+            )
+          }
+        )}
+      </main>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.section`
   padding: 1rem;
   @media screen and (min-width: 992px) {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    main {
+      width: 90vw;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+    }
   }
 `
-const FeaturedBlogs = () => {
-  return (
-    <Wrapper>
-      <Link to="/">
-        <Blog title="God is good" />
-      </Link>
-      <Link to="/">
-        <Blog title="God is good" />
-      </Link>
-      <Link to="/">
-        <Blog title="God is good" />
-      </Link>
-    </Wrapper>
-  )
-}
 
 export default FeaturedBlogs
