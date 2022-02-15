01707127d885096704ba710f008568e02667ac33
Eddiebee <edbeeotudor@gmail.com>
2022-02-15 21:01:32 +0100

    initial setup of error page

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/Layout"
+import styled from "styled-components"
+
+const Wrapper = styled.section`
+  min-height: calc(100vh - (6rem + 4rem));
+`
+
+const Error = () => {
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>en theos</h1>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default Error
